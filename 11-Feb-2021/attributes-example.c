82aabfe15d9aff253a77b371211fded1ae21ecf4
Ashwini Sahu <ashwinisahu990@gmail.com>
2021-02-11 13:05:33 +0530

    Thu Feb 11 01:05:33 PM IST 2021

---
+++ 11-Feb-2021/attributes-example.c
@@ -0,0 +1,16 @@
+#include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// To compile "gcc -lncurses prog.c"
+
+int main(int argc, char *argv[]){
+
+    int ch, prev, row, col;
+    prev = EOF;
+    FILE *fp;
+    int x, y;
+
+    if(argc !)
+
+}
---
+++ 11-Feb-2021/scanw-example.c
@@ -0,0 +1,26 @@
+#include <ncurses.h>
+#include <string.h>
+// To compile "gcc -lncurses prog.c"
+
+int main(){
+
+    char mesg[]="Enter a string : ";		/* message to be appeared on the screen */
+    char str[80];
+    int row,col;				/* to store the number of rows and *
+                        * the number of colums of the screen */
+    initscr();				/* start the curses mode */
+    getmaxyx(stdscr,row,col);		/* get the number of rows and columns */
+    mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
+                            /* print the message at the center of the screen */
+    getstr(str);
+    mvprintw(LINES - 2, 0, "You Entered : %s", str);
+    
+    attron(A_BOLD);
+    printw("\nYou Entered (In BOLD) : %s", str);
+    attroff(A_BOLD);
+    getch();
+    endwin();
+
+    return 0;
+
+}
